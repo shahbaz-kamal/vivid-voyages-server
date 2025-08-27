3f0cc02c1206faf7a1d33aacd40287d4d84417cd
Md. Shahbaz Kamal Chowdhury <shahbazkamal384@gmail.com>
2025-08-27 21:02:56 +0600

    server error handle implemented

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import { Server } from "http";
 
 import mongoose from "mongoose";
 import app from "./app";
+import { error } from "console";
 
 let server: Server;
 
@@ -21,3 +22,39 @@ const startServer = async () => {
 };
 
 startServer();
+
+//unhandled rejection
+process.on("unhandledRejection", (error) => {
+  console.log("unhandled rejection detected ...shutting down", error);
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+// Promise.reject(new Error("unhandled rejection!"));
+
+//uncaught exception
+process.on("uncaughtException", (error) => {
+  console.log("uncaught exception detected ...shutting down", error);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+});
+// throw new Error("I forgot to handle this");
+
+//Signal termination
+process.on("SIGTERM", () => {
+  console.log("object SIGTERM received...shutting down gracefully");
+  if (server) {
+    server.close(() => {
+      console.log("💥 Process terminated!");
+      process.exit(1);
+    });
+  }
+});
