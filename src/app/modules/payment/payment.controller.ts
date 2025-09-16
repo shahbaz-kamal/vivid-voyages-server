4bc4bbb0668e54d0655047b27cf5e50db7ce878a
Md. Shahbaz Kamal Chowdhury <shahbazkamal384@gmail.com>
2025-09-17 02:52:55 +0600

    payment integration complete

--- src/app/modules/payment/payment.controller.ts
+++ src/app/modules/payment/payment.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { catchAsync } from "../../utils/catchAsync";
 import { PaymentService } from "./payment.service";
 import { envVars } from "../../config/env";
+import { sendResponse } from "../../utils/sendResponse";
 
 const successPayment = catchAsync(async (req: Request, res: Response) => {
   const query = req.query;
@@ -14,5 +15,42 @@ const successPayment = catchAsync(async (req: Request, res: Response) => {
     );
   }
 });
+const failPayment = catchAsync(async (req: Request, res: Response) => {
+  const query = req.query;
+  const result = await PaymentService.failPayment(
+    query as Record<string, string>
+  );
+  if (result?.success === false) {
+    res.redirect(
+      `${envVars.SSL.SSL_FAIL_FRONTEND_URL}?transactionId=${query.transactionId}&message=${result.message}&amount=${query.amount}&status=${query.status}`
+    );
+  }
+});
+const cancelPayment = catchAsync(async (req: Request, res: Response) => {
+  const query = req.query;
+  const result = await PaymentService.cancelPayment(
+    query as Record<string, string>
+  );
+  if (result?.success === false) {
+    res.redirect(
+      `${envVars.SSL.SSL_FAIL_FRONTEND_URL}?transactionId=${query.transactionId}&message=${result.message}&amount=${query.amount}&status=${query.status}`
+    );
+  }
+});
+const initPayment = catchAsync(async (req: Request, res: Response) => {
+  const bookingId = req.params.bookingId;
+  const result = await PaymentService.initPayment(bookingId);
+  sendResponse(res, {
+    statusCode: 201,
+    success: true,
+    message: "Payment Done Successfully",
+    data: result,
+  });
+});
 
-export const PaymentController = { successPayment };
+export const PaymentController = {
+  successPayment,
+  failPayment,
+  cancelPayment,
+  initPayment,
+};
